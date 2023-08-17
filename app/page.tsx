e09718570a77fbb1bf372463e298cef70d6d2af3
chanseop <dlackstjq23@gmail.com>
2023-08-17 10:49:26 +0900

    feat:메인 사진 추가 및 footer 수정
    메인 사진 추가
    footer a 테그 img 테그 nextjs Link 테그와 Image 테그로 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import HomeImage from '@/components/ui/home/HomeImage'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='mt-10' style={{marginTop:'120px'}}>
-      Home
+    <main className='mt-[105px]' >
+      <h3 className='hidden'>Home</h3>
+      <HomeImage/>
     </main>
   )
 
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["m.shinsegaepoint.com"],
+    domains: ["m.shinsegaepoint.com","s3-alpha-sig.figma.com"],
   },
 };
 
